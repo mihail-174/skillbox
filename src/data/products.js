b1066bfa8d707599ab9d69189c8f681849c8c48e
Михаил <p.mihail174@yandex.ru>
2023-05-11 20:04:49 +0500

    3.6

---
+++ src/data/products.js
@@ -0,0 +1,57 @@
+export default [
+  {
+    title: 'Смартфон Honor X9a 6/128 ГБ',
+    price: '21376',
+    image: '/img/radio.jpg',
+  },
+  {
+    title: 'Смартфон Honor X8 6/128 ГБ, синий',
+    price: '15982',
+    image: '/img/wheels.jpg',
+  },
+  {
+    title: 'Смартфон Honor X8 6/128 ГБ, серебристый',
+    price: '21990',
+    image: 'https://ir-2.ozone.ru/s3/multimedia-s/wc700/6590863756.jpg',
+  },
+  {
+    title: 'Смартфон Xiaomi Redmi 12C 4/128 ГБ, синий',
+    price: '11499',
+    image: '/img/bicycle.jpg',
+  },
+  {
+    title: 'Смартфон Honor X7 4/128 ГБ, черный',
+    price: '11271',
+    image: '/img/headphones.jpg',
+  },
+  {
+    title: 'Смартфон Xiaomi Redmi Note 11S 6/64 ГБ, серый',
+    price: '13949',
+    image: 'https://ir-2.ozone.ru/s3/multimedia-s/wc700/6590863756.jpg',
+  },
+  {
+    title: 'Смартфон Honor X9a 6/128 ГБ, серебристый',
+    price: '22286',
+    image: 'https://ir-2.ozone.ru/s3/multimedia-s/wc700/6590863756.jpg',
+  },
+  {
+    title: 'Смартфон Xiaomi Redmi Note 11S 6/64 ГБ, синий',
+    price: '13949',
+    image: 'https://ir-2.ozone.ru/s3/multimedia-i/wc700/6623295966.jpg',
+  },
+  {
+    title: 'Смартфон Honor X7a 4/128 ГБ, синий',
+    price: '9234',
+    image: 'https://ir-2.ozone.ru/s3/multimedia-e/wc700/6590860358.jpg',
+  },
+  {
+    title: 'Смартфон Xiaomi Redmi Note 12 Pro 8/256 ГБ, серый',
+    price: '25019',
+    image: 'https://ir-2.ozone.ru/s3/multimedia-i/wc700/6623295966.jpg',
+  },
+  {
+    title: 'Смартфон Xiaomi Redmi Note 11 4/128 ГБ, синий',
+    price: '15891',
+    image: 'https://ir-2.ozone.ru/s3/multimedia-s/wc700/6590863756.jpg',
+  },
+];
